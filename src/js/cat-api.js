68b1de46d76f24864412dc8748bfb4c43548f52c
DariaRodovnichenko <DariaRodovnichenko@users.noreply.github.com>
2023-07-20 04:14:33 +0200

    first

---
+++ src/js/cat-api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_fwIQ0QcC8AuWHMcy6B6OLEXyaOpxUeIePhpPZ3fmZzcoLOUaCEuDr8CLgwAg0qkX';
+
+export function fetchBreeds() {
+  return axios
+    .get('https://api.thecatapi.com/v1/breeds')
+    .then(response => response.data)
+    .catch(error => {
+      console.error(error);
+      throw error;
+    });
+}
+
+export function fetchCatByBreed(breedId) {
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(response => response.data)
+    .catch(error => {
+      console.error(error);
+      throw error;
+    });
+}
